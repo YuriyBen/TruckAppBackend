bf5f34c5f1f56e38c9e53c7c82b6ce3ed3f62766
YuriyBen <yuraben200159@gmail.com>
2020-05-15 22:30:45 +0300

    add validations

--- TruckProject/ResourceParameters/UserAuthentication.cs
+++ TruckProject/ResourceParameters/UserAuthentication.cs
@@ -9,13 +9,18 @@ namespace TruckProject.ResourceParameters
 {
     public class UserAuthentication
     {
+        [RegularExpression(@"^[a-zA-Z]+$")]
         public string FirstName { get; set; }
+        [RegularExpression(@"^[a-zA-Z]+$")]
         public string LastName { get; set; }
-        //[Unique]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [PasswordValidation]
         public string Password { get; set; }
         [Compare("Password")]
         public string ConfirmPassword { get; set; }
+        [Url]
         public string ImagePath { get; set; }
         public DateTime DateOfBirth { get; set; } //mm-dd-yy
         public string Sex { get; set; }
